b4f9b73df0e987e4e13dc93df8a7a774ee29d67f
ddienu <rockdann@hotmail.com>
2025-06-16 22:27:43 -0500

    feat: Warrior power number validation in update DTO

--- src/main/java/com/diegonunez/warriors/dto/Request/WarriorPowerUpdateDTO.java
+++ src/main/java/com/diegonunez/warriors/dto/Request/WarriorPowerUpdateDTO.java
@@ -1,12 +1,15 @@
 package com.diegonunez.warriors.dto.Request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public class WarriorPowerUpdateDTO {
     @NotNull(message = "Power's list cannot be null")
+    @Size(max = 5, min = 5, message="The warrior must have five powers")
     private List<Integer> powerIds;
 
     public WarriorPowerUpdateDTO(List<Integer> powerIds){
