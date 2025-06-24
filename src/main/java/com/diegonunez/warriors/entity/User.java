988d685bc5079cc7de2d82956fcd9dff6592bc9e
ddienu <rockdann@hotmail.com>
2025-06-24 11:30:04 -0500

    feat: Adding constructor, getters and setters

--- src/main/java/com/diegonunez/warriors/entity/User.java
+++ src/main/java/com/diegonunez/warriors/entity/User.java
@@ -3,6 +3,7 @@ package com.diegonunez.warriors.entity;
 import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,4 +16,54 @@ public class User {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "role_id", nullable = false)
     private Role role;
+
+    public User(Integer userId, String email, String password, UserStatus userstatus, Role role){
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.userStatus = userstatus;
+        this.role = role;
+    }
+
+    //Getters and setters
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserStatus getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(UserStatus userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 }
