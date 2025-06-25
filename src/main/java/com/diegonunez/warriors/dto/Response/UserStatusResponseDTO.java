de5c34f7498b6e5f7633e356ee8fbaf3ed14bdfa
ddienu <rockdann@hotmail.com>
2025-06-25 02:03:56 -0500

    feat: User status response DTO

--- src/main/java/com/diegonunez/warriors/dto/Response/UserStatusResponseDTO.java
+++ src/main/java/com/diegonunez/warriors/dto/Response/UserStatusResponseDTO.java
@@ -1,2 +1,38 @@
-package com.diegonunez.warriors.dto.Response;public class UserStatusResponseDTO {
+package com.diegonunez.warriors.dto.Response;
+
+public class UserStatusResponseDTO {
+    private Integer id;
+    private String name;
+    private String description;
+
+    public UserStatusResponseDTO(Integer id, String name, String description){
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    //Getters and Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
