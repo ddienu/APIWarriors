ba0b615cd8526d02e6279046e99acd0f128f1ae0
ddienu <rockdann@hotmail.com>
2025-06-25 01:43:17 -0500

    feat: Adding role controller interface

--- src/main/java/com/diegonunez/warriors/controller/IRoleController.java
+++ src/main/java/com/diegonunez/warriors/controller/IRoleController.java
@@ -1,2 +1,17 @@
-package com.diegonunez.warriors.controller;public interface IRoleController {
+package com.diegonunez.warriors.controller;
+
+import com.diegonunez.warriors.common.ApiResponse;
+import com.diegonunez.warriors.dto.Request.RoleRequestDTO;
+import com.diegonunez.warriors.dto.Response.RoleResponseDTO;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface IRoleController {
+    ResponseEntity<ApiResponse<RoleResponseDTO>> getRoleById(Integer roleId);
+    ResponseEntity<ApiResponse<List<RoleResponseDTO>>> getRoles();
+    ResponseEntity<ApiResponse<RoleResponseDTO>> createRole(RoleRequestDTO newRole);
+    ResponseEntity<ApiResponse<RoleResponseDTO>> updateRole(Integer roleId, RoleRequestDTO roleUpdated);
+    ResponseEntity<ApiResponse<Boolean>> deleteRole(Integer roleId);
+
 }
