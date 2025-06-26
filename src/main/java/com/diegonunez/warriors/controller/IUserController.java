9756a08ac29ab67c6faafb6fcc4b5ddc2a64905c
ddienu <rockdann@hotmail.com>
2025-06-26 00:36:05 -0500

    feat: User controller interface creation

--- src/main/java/com/diegonunez/warriors/controller/IUserController.java
+++ src/main/java/com/diegonunez/warriors/controller/IUserController.java
@@ -1,2 +1,16 @@
-package com.diegonunez.warriors.controller;public interface IUserController {
+package com.diegonunez.warriors.controller;
+
+import com.diegonunez.warriors.common.ApiResponse;
+import com.diegonunez.warriors.dto.Request.UserRequestDTO;
+import com.diegonunez.warriors.dto.Response.UserResponseDTO;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface IUserController {
+    ResponseEntity<ApiResponse<List<UserResponseDTO>>> findAllUsers();
+    ResponseEntity<ApiResponse<UserResponseDTO>> findUserById(Integer userId);
+    ResponseEntity<ApiResponse<UserResponseDTO>> createUser(UserRequestDTO newUser);
+    ResponseEntity<ApiResponse<UserResponseDTO>> updateUser(Integer userId, UserRequestDTO userUpdated);
+    ResponseEntity<ApiResponse<Boolean>> deleteUser(Integer userId);
 }
