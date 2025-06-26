d11fd6158cb9cd3198cf567cbc2dd08a14adab60
ddienu <rockdann@hotmail.com>
2025-06-26 00:34:31 -0500

    feat: User service interface creation

--- src/main/java/com/diegonunez/warriors/service/IUserService.java
+++ src/main/java/com/diegonunez/warriors/service/IUserService.java
@@ -1,2 +1,14 @@
-package com.diegonunez.warriors.service;public interface IUserService {
+package com.diegonunez.warriors.service;
+
+import com.diegonunez.warriors.dto.Request.UserRequestDTO;
+import com.diegonunez.warriors.dto.Response.UserResponseDTO;
+
+import java.util.List;
+
+public interface IUserService {
+    UserResponseDTO findUserById(Integer userId);
+    List<UserResponseDTO> findAllUsers();
+    UserResponseDTO createUser(UserRequestDTO newUser);
+    UserResponseDTO updateUser(Integer userId, UserRequestDTO userUpdated);
+    Boolean deleteUser(Integer userId);
 }
