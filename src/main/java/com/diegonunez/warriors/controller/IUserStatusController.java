5a7eb744a5d4a04c993021b036aaafc6c9c04585
ddienu <rockdann@hotmail.com>
2025-06-25 23:19:11 -0500

    feat: User status controller interface creation

--- src/main/java/com/diegonunez/warriors/controller/IUserStatusController.java
+++ src/main/java/com/diegonunez/warriors/controller/IUserStatusController.java
@@ -1,2 +1,16 @@
-package com.diegonunez.warriors.controller;public interface IUserStatusController {
+package com.diegonunez.warriors.controller;
+
+import com.diegonunez.warriors.common.ApiResponse;
+import com.diegonunez.warriors.dto.Request.UserStatusRequestDTO;
+import com.diegonunez.warriors.dto.Response.UserStatusResponseDTO;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface IUserStatusController {
+    ResponseEntity<ApiResponse<List<UserStatusResponseDTO>>> findAllUserStatus();
+    ResponseEntity<ApiResponse<UserStatusResponseDTO>> findUserStatusById(Integer userStatusId);
+    ResponseEntity<ApiResponse<UserStatusResponseDTO>> createUserStatus(UserStatusRequestDTO newUserStatus);
+    ResponseEntity<ApiResponse<UserStatusResponseDTO>> updateUserStatus(Integer userStatusId, UserStatusRequestDTO userStatusUpdated);
+    ResponseEntity<ApiResponse<Boolean>> deleteUserStatus(Integer userStatusId);
 }
