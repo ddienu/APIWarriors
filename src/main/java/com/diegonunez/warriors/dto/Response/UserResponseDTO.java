dc1f797e5b2fee395e65974e93b2246ba6340412
ddienu <rockdann@hotmail.com>
2025-06-26 00:33:51 -0500

    feat: User response DTO implementation

--- src/main/java/com/diegonunez/warriors/dto/Response/UserResponseDTO.java
+++ src/main/java/com/diegonunez/warriors/dto/Response/UserResponseDTO.java
@@ -1,2 +1,58 @@
-package com.diegonunez.warriors.dto.Response;public class UserResponseDTO {
+package com.diegonunez.warriors.dto.Response;
+
+public class UserResponseDTO {
+
+    private Integer userId;
+    private String email;
+    private String password;
+    private UserStatusResponseDTO userStatus;
+    private RoleResponseDTO role;
+
+    public UserResponseDTO(Integer userId, String email, String password, UserStatusResponseDTO userStatus, RoleResponseDTO role){
+        this.userId = userId;
+        this.email = email;
+        this.password = password;
+        this.userStatus = userStatus;
+        this.role = role;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public UserStatusResponseDTO getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(UserStatusResponseDTO userStatus) {
+        this.userStatus = userStatus;
+    }
+
+    public RoleResponseDTO getRole() {
+        return role;
+    }
+
+    public void setRole(RoleResponseDTO role) {
+        this.role = role;
+    }
 }
