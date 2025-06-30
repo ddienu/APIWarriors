31813fa2a794f0e46f8455f64483e869fe3fbe4c
ddienu <rockdann@hotmail.com>
2025-06-30 00:55:34 -0500

    refactor: Making email field unique into user entity

--- src/main/java/com/diegonunez/warriors/entity/User.java
+++ src/main/java/com/diegonunez/warriors/entity/User.java
@@ -8,6 +8,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer userId;
+    @Column(unique = true)
     private String email;
     private String password;
     @ManyToOne(fetch = FetchType.LAZY)
